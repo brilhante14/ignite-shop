35f8f86dbf1fd399d0009026c01e7ec09e45e986
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-02-06 10:43:51 -0300

    style: Styling products panel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,32 @@
+import Image from "next/image";
 import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import shirt1 from '../assets/merch/1.png';
+import shirt2 from '../assets/merch/2.png';
+import shirt3 from '../assets/merch/3.png';
 
 export default function Home() {
   return (
-    <Button>
-      Submit
-      <span> ASAP</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="Product 1" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="Product 2" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-});
